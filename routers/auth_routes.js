7b0c816f6ebc43d56291b2a32a163d7cf2d813e6
Manoj Neupane <manpane98@gmail.com>
2023-04-28 09:53:40 +0545

    installed required libraries

--- routers/auth_routes.js
+++ routers/auth_routes.js
@@ -3,5 +3,16 @@ express = require("express")
 const router = express.Router()
 const User = require("../model/user")
 
+router.post("/signup",async (req,res) => {
+
+
+})
+
+router.post("/login",async (req,res) => {
+    
+
+})
+
+
 
 module.exports = router;
